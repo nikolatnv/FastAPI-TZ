f28938766708f84fc195d1c463d84c70ecddf943
basay <nikolatnv@gmail.com>
2024-02-17 23:19:06 +0300

    two

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import uuid
 from aiofile import async_open
 
 # Подключение к базе данных PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://root:example@postgres/test_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://root:example@db/test_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Создание сессии SQLAlchemy
@@ -88,3 +88,5 @@ async def download(uuid: str):
         return FileResponse(content, filename=uuid)
     else:
         raise HTTPException(status_code=404, detail="File not found")
+
+
